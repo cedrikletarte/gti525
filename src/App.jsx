14c62e6dceeb346a5dac42245b55db12fad11f06
cedrikletarte <cedrikletarte@gmail.com>
2026-07-09 09:56:29 -0400

    fix : rename file Statistic to Statistique

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './theme';
 import HomePage from './pages/HomePage';
-import Statistic from './pages/Statistic';
+import Statistique from './pages/Statistique';
 import About from './pages/About';
 import Reseau from './pages/Reseau';
 import PointInteret from './pages/PointInteret';
@@ -15,7 +15,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/statistiques" element={<Statistic />} />
+          <Route path="/statistiques" element={<Statistique />} />
           <Route path="/points-interet" element={<PointInteret />} />
           <Route path="/assistant" element={<Assistant />} />
           <Route path="/a-propos" element={<About />} />
